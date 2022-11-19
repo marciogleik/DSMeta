06d5e95748ed9fd9c9c27c1e64a5a375720f889e
Marcio Gleik <marcio.junioor@gmail.com>
2022-11-19 12:40:50 -0300

    Fisrt component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButton from "./componentes/NotificatinButton"
+
 function App() {
   return(
+    <>
     <h1>Olá mundo!</h1>
+    <NotificationButton />
+    </>
   )
 }
 
---
+++ frontend/src/componentes/NotificatinButton/index.tsx
@@ -0,0 +1,13 @@
+import botao from '../../assets/img/notification-icon.svg'
+
+import './styles.css'
+
+function NotificationButton() {
+    return (
+        <div className="dsmeta-red-btn">
+            <img src={botao} alt="Notificar" />
+        </div>
+    )
+}
+
+export default NotificationButton
