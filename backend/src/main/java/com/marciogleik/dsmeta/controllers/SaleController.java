7d67a224c04552fffaa511fbbaecf1c4fd343045
Marcio Gleik <marcio.junioor@gmail.com>
2022-11-19 18:04:11 -0300

    Date select

--- backend/src/main/java/com/marciogleik/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/marciogleik/dsmeta/controllers/SaleController.java
@@ -1,10 +1,11 @@
 package com.marciogleik.dsmeta.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.marciogleik.dsmeta.entities.Sale;
@@ -19,7 +20,10 @@ public class SaleController {
 	
 	
 	@GetMapping
-	public List<Sale> findSales() {
-		return service.findSales();
+	public Page<Sale> findSales(
+			@RequestParam(value="minDate", defaultValue = "") String minDate,
+			@RequestParam(value="maxDate", defaultValue = "") String maxDate,
+			Pageable pageable) {
+		return service.findSales(minDate, maxDate, pageable);
 	}
 }
