5724155996eca3ca9325bc3c6512d3b0e7e7d411
Marcio Gleik <marcio.junioor@gmail.com>
2022-11-20 10:30:20 -0300

    Date update

--- frontend/src/componentes/SalesCard/index.tsx
+++ frontend/src/componentes/SalesCard/index.tsx
@@ -18,11 +18,15 @@ function SalesCard() {
     const [maxDate, setMaxDate] = useState(max);
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales`)
+
+        const dmin = minDate.toISOString().slice(0, 10);
+        const dmax = maxDate.toISOString().slice(0, 10);
+
+        axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
             .then(response => {
                 setSales(response.data.content);
             });
-    }, []);
+    }, [minDate, maxDate]);
 
     return (
         <div className="dsmeta-card">
